6445689eb620768bb1dc1869e5212b261d7968fd
artoma <artoma@gmail.com>
2023-04-26 15:27:06 +0300

    feat: content sec. policy

--- myApp.js
+++ myApp.js
@@ -10,7 +10,8 @@ app.use(helmet.ieNoOpen());
 const timeInSeconds = 90*24*60*60;
 app.use( helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache())
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({scriptSrc: ["'self'", "'trusted-cdn.com'"]}))
 
 
 
